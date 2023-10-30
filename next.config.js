d24c0ae391b08c2d004140c2fb8a5710e5b3940b
rk-akaike <ramakrishna@akaiketech.com>
2023-10-30 10:06:21 +0530

    Cache the svg's.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)\\.svg",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400", // Cache SVGs for 1 day (86400 seconds)
+          },
+          {
+            key: "Expires",
+            value: new Date(Date.now() + 86400000).toUTCString(), // Set an expiration date 1 day from now
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

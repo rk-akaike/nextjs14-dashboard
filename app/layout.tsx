7060c40a42a41fa5f7a5a330ea30e4d8e1884aad
rk-akaike <ramakrishna@akaiketech.com>
2024-01-09 14:16:09 +0530

    Add Clarity analytics script to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { inter } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
 
 import { Metadata } from "next";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: {
@@ -22,6 +23,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "kj29sd6u1x");
+      `,
+          }}
+        />
+      </Head>
       <body className={`${inter.className} antialiased`}>
         {children}
         <Analytics />

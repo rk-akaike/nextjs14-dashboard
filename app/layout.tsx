17b5121b3170617a956ab65b1d383cc303eb1db0
rk-akaike <ramakrishna@akaiketech.com>
2024-01-09 15:13:12 +0530

    Replace Script component with Head component for clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { inter } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
 
 import { Metadata } from "next";
-import Script from "next/script";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: {
@@ -23,16 +23,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Script strategy="lazyOnload" id="clarity-script">
-        {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "kj29sd6u1x");
-        `}
-      </Script>
-
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "kj29sd6u1x");
+      `,
+          }}
+        />
+      </Head>
       <body className={`${inter.className} antialiased`}>
         {children}
         <Analytics />

08e3b5896c08e617767fdeab058b96b1657baf96
rk-akaike <ramakrishna@akaiketech.com>
2024-01-12 12:39:14 +0530

    Update clarity script import in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { inter } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
 
 import { Metadata } from "next";
-import Head from "next/head";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -23,19 +23,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "kj29sd6u1x");
-      `,
-          }}
-        />
-      </Head>
+      <Script strategy="lazyOnload" id="clarity-script">
+        {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "kj29sd6u1x");
+        `}
+      </Script>
+
       <body className={`${inter.className} antialiased`}>
         {children}
         <Analytics />
